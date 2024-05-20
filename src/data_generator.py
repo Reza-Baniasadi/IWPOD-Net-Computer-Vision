752cd3f255e89b768de427ec5d269d90d357f056
Mac <redsbaniasadi1381@gmail.com>
2024-05-20 10:01:00 +0330

    add atrebute for class

--- src/data_generator.py
+++ src/data_generator.py
@@ -4,3 +4,12 @@ from tensorflow import keras
 from src.sampler import augment_sample, labels2output_map
 
 class ALPRDataGenerator(keras.utils.Sequence):
+     def __init__(self, data, batch_size=32, dim =  208, stride = 16, shuffle=True, OutputScale = 1.0):
+        'Initialization'
+        self.dim = dim
+        self.stride = stride
+        self.batch_size = batch_size
+        self.data = data
+        self.shuffle = shuffle
+        self.OutputScale = OutputScale
+        self.on_epoch_end()
