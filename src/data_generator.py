c962089ca7602f943183ec832ca0c3f7fea171f0
Mac <redsbaniasadi1381@gmail.com>
2024-05-21 10:08:00 +0330

    random data

--- src/data_generator.py
+++ src/data_generator.py
@@ -16,3 +16,11 @@ class ALPRDataGenerator(keras.utils.Sequence):
 
      def __len__(self):
         return int(np.ceil(len(self.data) / self.batch_size))
+     
+     def __getitem__(self, index):
+		
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        X, y = self.__data_generation(indexes)
+
+        return X, y
