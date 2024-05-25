4d1479e74066c2cfad0f4a081c8905f261ed84b6
Mac <redsbaniasadi1381@gmail.com>
2024-05-25 10:08:00 +0500

    get vechicle

--- src/Detection.py
+++ src/Detection.py
@@ -5,3 +5,10 @@ from src.utils 			import im2single, nms_darkflow, nms_darkflow_target, adjust_pt
 from src.drawing_utils	import draw_losangle
 
 
+def detect_vechicle(tfnet_yolo, imgcv):
+	result = tfnet_yolo.return_predict(imgcv)
+	vehicles = []
+	for det in result:
+		if (det['label'] in ['car','bus']):
+			vehicles.append(det)
+	return vehicles
