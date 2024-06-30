66897f5b9715d2ffcd9406d478dd6a5b2f85779a
Mac <redsbaniasadi1381@gmail.com>
2024-07-01 01:26:00 +0330

    draw losangle

--- src/drawing_utils.py
+++ src/drawing_utils.py
@@ -8,3 +8,11 @@ def draw_label(I,l,color=(255,0,0),thickness=1):
 	tl = tuple((l.tl()*wh).astype(int).tolist())
 	br = tuple((l.br()*wh).astype(int).tolist())
 	cv2.rectangle(I,tl,br,color,thickness=thickness)
+	
+def draw_losangle(I,pts,color=(1.,1.,1.),thickness=1):
+	assert(pts.shape[0] == 2 and pts.shape[1] == 4)
+
+	for i in range(4):
+		pt1 = tuple(pts[:,i].astype(int).tolist())
+		pt2 = tuple(pts[:,(i+1)%4].astype(int).tolist())
+		cv2.line(I,pt1,pt2,color,thickness)
