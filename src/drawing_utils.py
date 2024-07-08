cabb0f8db3e7d4418019989c24673a478fce534b
Mac <redsbaniasadi1381@gmail.com>
2024-07-09 01:26:00 +0330

    write image

--- src/drawing_utils.py
+++ src/drawing_utils.py
@@ -16,3 +16,15 @@ def draw_losangle(I,pts,color=(1.,1.,1.),thickness=1):
 		pt1 = tuple(pts[:,i].astype(int).tolist())
 		pt2 = tuple(pts[:,(i+1)%4].astype(int).tolist())
 		cv2.line(I,pt1,pt2,color,thickness)
+		
+def write2img(Img,label,strg,txt_color=(0,0,0),bg_color=(255,255,255),font_size=1):
+	wh_img = np.array(Img.shape[1::-1])
+
+	font = cv2.FONT_HERSHEY_SIMPLEX
+
+	wh_text,v = cv2.getTextSize(strg, font, font_size, 3)
+	bl_corner = label.tl()*wh_img
+
+	tl_corner = np.array([bl_corner[0],bl_corner[1]-wh_text[1]])/wh_img
+	br_corner = np.array([bl_corner[0]+wh_text[0],bl_corner[1]])/wh_img
+	bl_corner /= wh_img
