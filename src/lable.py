fdd60b11a6dc4b86f9a013283fc6bebd4aafa3d0
Mac <redsbaniasadi1381@gmail.com>
2024-07-14 08:02:00 +0330

    def add

--- src/lable.py
+++ src/lable.py
@@ -5,11 +5,21 @@ from os.path import isfile
 
 class Label:
 
-	def __init__(self,cl=-1,tl=np.array([0.,0.]),br=np.array([0.,0.]),prob=None):
-		self.__tl 	= tl
-		self.__br 	= br
-		self.__cl 	= cl
-		self.__prob = prob
+        def __init__(self,cl=-1,tl=np.array([0.,0.]),br=np.array([0.,0.]),prob=None):
+            self.__tl 	= tl
+            self.__br 	= br
+            self.__cl 	= cl
+            self.__prob = prob
 
-	def __str__(self):
-		return 'Class: %d, top_left(x:%f,y:%f), bottom_right(x:%f,y:%f)' % (self.__cl, self.__tl[0], self.__tl[1], self.__br[0], self.__br[1])
+        def __str__(self):
+            return 'Class: %d, top_left(x:%f,y:%f), bottom_right(x:%f,y:%f)' % (self.__cl, self.__tl[0], self.__tl[1], self.__br[0], self.__br[1])
+        def copy(self):
+            return Label(self.__cl,self.__tl,self.__br)
+
+        def wh(self): return self.__br-self.__tl
+
+        def cc(self): return self.__tl + self.wh()/2
+
+        def tl(self): return self.__tl
+
+        def br(self): return self.__br
