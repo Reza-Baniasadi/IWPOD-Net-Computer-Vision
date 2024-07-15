750fdd1b36019f8626c6d8d73e7b334bd4482861
Mac <redsbaniasadi1381@gmail.com>
2024-07-15 08:24:00 +0330

    def tl wh prob add

--- src/lable.py
+++ src/lable.py
@@ -23,7 +23,7 @@ class Label:
         def tl(self): return self.__tl
 
         def br(self): return self.__br
-        
+
         def tr(self): return np.array([self.__br[0],self.__tl[1]])
 
         def bl(self): return np.array([self.__tl[0],self.__br[1]])
@@ -33,3 +33,20 @@ class Label:
         def area(self): return np.prod(self.wh())
 
         def prob(self): return self.__prob
+
+        def set_class(self,cl):
+            self.__cl = cl
+
+        def set_tl(self,tl):
+            self.__tl = tl
+
+        def set_br(self,br):
+            self.__br = br
+
+        def set_wh(self,wh):
+            cc = self.cc()
+            self.__tl = cc - .5*wh
+            self.__br = cc + .5*wh
+
+        def set_prob(self,prob):
+            self.__prob = prob
