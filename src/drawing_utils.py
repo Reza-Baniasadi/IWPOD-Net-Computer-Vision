c97ee565911eb3e3cc1d16d1149cc2663567cb20
Mac <redsbaniasadi1381@gmail.com>
2024-07-29 10:26:00 +0330

    add corner

--- src/drawing_utils.py
+++ src/drawing_utils.py
@@ -28,3 +28,22 @@ def write2img(Img,label,strg,txt_color=(0,0,0),bg_color=(255,255,255),font_size=
 	tl_corner = np.array([bl_corner[0],bl_corner[1]-wh_text[1]])/wh_img
 	br_corner = np.array([bl_corner[0]+wh_text[0],bl_corner[1]])/wh_img
 	bl_corner /= wh_img
+
+	if (tl_corner < 0.).any():
+			delta = 0. - np.minimum(tl_corner, 0.)
+
+	elif (br_corner > 1.).any():
+			delta = 1. - np.maximum(br_corner, 1.)
+
+							
+	else:
+			delta = 0.
+
+	tl_corner += delta
+	br_corner += delta
+	bl_corner += delta
+
+	tpl = lambda x: tuple((x*wh_img).astype(int).tolist())
+
+	cv2.rectangle(Img, tpl(tl_corner), tpl(br_corner), bg_color, -1)	
+	cv2.putText(Img,strg,tpl(bl_corner),font,font_size,txt_color,3)
