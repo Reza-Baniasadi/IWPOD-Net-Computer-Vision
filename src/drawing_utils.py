699a48a55ffadaee791975111205a8508386024f
Mac <redsbaniasadi1381@gmail.com>
2024-07-31 10:26:00 +0330

    to int

--- src/drawing_utils.py
+++ src/drawing_utils.py
@@ -4,7 +4,7 @@ import cv2
 
 
 def draw_label(I,l,color=(255,0,0),thickness=1):
-	wh = np.array(I.shape[1::-1]).astype(float)
+	wh = np.array(I.shape[1::-1]).astype(int)
 	tl = tuple((l.tl()*wh).astype(int).tolist())
 	br = tuple((l.br()*wh).astype(int).tolist())
 	cv2.rectangle(I,tl,br,color,thickness=thickness)
