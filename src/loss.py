6cce46811a8acc340bc1a038657507e1bb768475
Mac <redsbaniasadi1381@gmail.com>
2024-08-07 02:25:00 +0000

    add location loss

--- src/loss.py
+++ src/loss.py
@@ -36,3 +36,37 @@ def clas_loss(Ytrue, Ypred):
 	res  = wtrue*logloss(obj_probs_true,obj_probs_pred,(b,h,w,1))
 	res  += wfalse*logloss(non_obj_probs_true,non_obj_probs_pred,(b,h,w,1))
 	return res
+
+
+
+def loc_loss(Ytrue, Ypred):
+
+	b = tf.shape(Ytrue)[0]
+	h = tf.shape(Ytrue)[1]
+	w = tf.shape(Ytrue)[2]
+
+	obj_probs_true = Ytrue[...,0]
+	affine_pred	= Ypred[...,1:]
+	pts_true 	= Ytrue[...,1:]
+
+	affinex = tf.stack([tf.maximum(affine_pred[...,0],0.),affine_pred[...,1],affine_pred[...,2]],3)
+	affiney = tf.stack([affine_pred[...,3],tf.maximum(affine_pred[...,4],0.),affine_pred[...,5]],3)
+
+	v = 0.5
+	base = tf.stack([[[[-v,-v,1., v,-v,1., v,v,1., -v,v,1.]]]])
+	base = tf.tile(base,tf.stack([b,h,w,1]))
+
+	pts = tf.zeros((b,h,w,0))
+
+	for i in range(0, 12, 3):
+		row = base[...,i:(i+3)]
+		ptsx = tf.reduce_sum(affinex*row,3)
+		ptsy = tf.reduce_sum(affiney*row,3)
+
+		pts_xy = tf.stack([ptsx,ptsy],3)
+		pts = (tf.concat([pts,pts_xy],3))
+
+	flags = tf.reshape(obj_probs_true, (b,h,w,1))
+	#dimmax = 13
+	res   =  1.0*l1(pts_true*flags, pts*flags, (b, h, w, 4*2))
+	return res
