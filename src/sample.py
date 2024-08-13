79a4f600ced8d44040f202ba17a24fdccfb233f4
Mac <redsbaniasadi1381@gmail.com>
2024-08-13 08:03:00 +0000

    create list

---
+++ src/sample.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import random
+import glob
+
+from src.utils 	import im2single, getWH, hsv_transform
+from src.label	import Label
+from src.projection_utils import perspective_transform, find_T_matrix, getRectPts
+
+UseBG = True
+bgimages = []
+dim0 = 208
+BGDataset = 'bgimages\\'
+
+
+if UseBG:
+	imglist = glob.glob(BGDataset + '*.jpg')
+	for im in imglist:
+		img = cv2.imread(im)
+		factor = max(1, dim0/min(img.shape[0:2]))
+		img = cv2.resize(img, (0,0), fx = factor, fy = factor).astype('float32')/255
+		bgimages.append(img)
